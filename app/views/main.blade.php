bec974c8ccd14c9a977637ce5b487825aaed19c9
Rebecca Mazur <mazurr@Rebeccas-MacBook-Pro.local>
2014-11-29 09:15:47 -0500

    Errors on live server

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -3,7 +3,65 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Fantasy Language Builder</title>
-	<link rel="stylesheet" type="text/css" href="http://p4.cscie15.theredsetter.com/css/styles.css" />
+	<style type = "text/css">
+	
+	body
+{
+	text-align: left;
+}
+
+h1 
+{
+	font-size: 32px;
+	margin: 16px 0 0 0;
+}
+
+table
+{
+	width: 100%;
+	border-collapse: collapse;
+}
+
+td, th
+{
+	border: 1px solid black;
+	padding: 1em;
+	vertical-align: top;
+}
+
+#navigation
+{
+	width: 24%;
+	float: left;
+}
+
+#content
+{
+	width: 74%;
+	float: right;
+}
+
+.columns
+{    
+	-webkit-column-count: 5; 
+	-moz-column-count: 5;
+	column-count: 5;
+}     
+
+
+.columns p
+{
+	margin-top: 0;
+	padding-top: 0;
+}
+
+.alert
+{
+	font-weight: bold;
+	color: red;
+}
+	
+	</style>
 
 </head>
 <body>
