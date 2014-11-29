dce3336f69d4808ac66d1dd22359775ab2c1ab17
Rebecca Mazur <mazurr@Rebeccas-MacBook-Pro.local>
2014-11-29 09:14:33 -0500

    Errors on live server

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Fantasy Language Builder</title>
-	<link rel="stylesheet" type="text/css" href="/css/styles.css" />
+	<link rel="stylesheet" type="text/css" href="http://p4.cscie15.theredsetter.com/css/styles.css" />
 
 </head>
 <body>
